0c5a70c0aeff49b1868116fdc2b51cc58fc43a0d
Ryan Gibb <ryan@freumh.org>
2023-02-14 14:42:40 +0000

    nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    opam-nix.url = "github:tweag/opam-nix";
+    flake-utils.url = "github:numtide/flake-utils";
+    opam-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { self, nixpkgs, flake-utils, opam-nix }@inputs:
+    let package = "ipaddr";
+    in flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        opam-nix-lib = opam-nix.lib.${system};
+        devPackagesQuery = {
+          ocaml-lsp-server = "*";
+          ocamlformat = "*";
+        };
+        resolved-scope = opam-nix-lib.buildOpamProject' { } ./. devPackagesQuery;
+      in rec {
+        packages = resolved-scope // { default = resolved-scope.${package}; };
+        defaultPackage = packages.default;
+      });
+}
